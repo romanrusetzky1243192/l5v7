95cf50c3d539891a9a58adee3434cb7df21ab21f
roma <roman28682972@gmail.com>
2024-10-25 18:12:59 +0500

    добавил активность

---
+++ app/src/main/java/com/example/myapplication/CurrencyActivity.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class CurrencyActivity: AppCompatActivity() {
+}
