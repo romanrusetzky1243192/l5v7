c0676b347e1573153286f090a3c0354bea2d981d
roma <roman28682972@gmail.com>
2024-10-25 18:17:58 +0500

    доработал основную логику

--- app/src/main/java/com/example/myapplication/CurrencyActivity.kt
+++ app/src/main/java/com/example/myapplication/CurrencyActivity.kt
@@ -1,7 +1,17 @@
 package com.example.myapplication
+
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class CurrencyActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_currency)
+
+        val Text: TextView = findViewById(R.id.payment_text)
+        val totalPrice = intent.getIntExtra("totalPrice", 0)
+
+        Text.text = "Цена = $totalPrice рублей"
+    }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,62 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var quantityEditText: EditText
+    private lateinit var currencySpinner: Spinner
+
+    private val currencyRates = mapOf(
+        "Доллар" to 75,
+        "Евро" to 90,
+        "Тонна" to 100
+    )
+    private var selectedRate = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        quantityEditText = findViewById(R.id.quantity_edit_text)
+        currencySpinner = findViewById(R.id.currency_spinner)
+        val calculateButton: Button = findViewById(R.id.calculate_button)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, currencyRates.keys.toList())
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        currencySpinner.adapter = adapter
+
+        currencySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                val selectedItem = parent.getItemAtPosition(position) as String
+                selectedRate = currencyRates[selectedItem] ?: 0
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                selectedRate = 0
+            }
+        }
+        calculateButton.setOnClickListener {
+            val quantityString = quantityEditText.text.toString()
+            if (quantityString.isNotEmpty()) {
+                val quantity = quantityString.toIntOrNull()
+                if (quantity != null) {
+                    val totalPrice = selectedRate * quantity
+                    val intent = Intent(this, CurrencyActivity::class.java)
+                    intent.putExtra("totalPrice", totalPrice)
+                    startActivity(intent)
+                } else {
+                    quantityEditText.error = "Пожалуйста, введите корректное число"
+                }
+            } else {
+                quantityEditText.error = "Введите количество"
+            }
         }
     }
 }
